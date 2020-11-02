e13a2d62ef7c872ec0b00797c9142d96e642a076
Edwin Mejia <60832962+edan505@users.noreply.github.com>
2020-11-01 19:47:06 -0600

    Accordion FAQ Design update
    1.) Added the ability to select multiple FAQ-cards simultaneously.
    2.) Main-div's height expands based on the amount of collapsed FAQ-cards
    3.) Changed background color to a light gray for all <hr> tags
    4.) Fixed issue where the top picture was not visible while on a mobile viewport.

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ let illustrationBox = document.querySelector(".illustration-box");
 
 for (let i = 0; i <= faqComponents.length - 1; i++) {
     faqComponents[i].addEventListener("click", function () {
-        for (let k = 0; k <= faqComponents.length - 1; k++) {
-            if (k != i) {
-                questionTexts[k].classList.remove("question-text-clicked");
-                buttonDivs[k].classList.remove("button-div-clicked");
-                answerCards[k].classList.remove("hidden-answer-click");
-            }
-        }
+       
         questionTexts[i].classList.toggle("question-text-clicked");
         buttonDivs[i].classList.toggle("button-div-clicked");
         answerCards[i].classList.toggle("hidden-answer-click");
